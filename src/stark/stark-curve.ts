54b5a0ceaf70f598bc4e5d2eabf23cf6b1e46db7
Bruno Silva <100642763+bruno-imx@users.noreply.github.com>
2022-04-21 13:10:50 +1000

    Adds base sdk functions

---
+++ src/stark/stark-curve.ts
@@ -0,0 +1,33 @@
+import * as encUtils from 'enc-utils';
+import BN from 'bn.js';
+import assert from 'assert';
+import { ec } from 'elliptic';
+
+export function getIntFromBits(
+    hex: string,
+    start: number,
+    end: number | undefined = undefined,
+): number {
+    const bin = encUtils.hexToBinary(hex);
+    const bits = bin.slice(start, end);
+    const int = encUtils.binaryToNumber(bits);
+    return int;
+}
+
+export function fixMessage(msg: string) {
+    msg = encUtils.removeHexPrefix(msg);
+    msg = new BN(msg, 16).toString(16);
+
+    if (msg.length <= 62) {
+        // In this case, msg should not be transformed, as the byteLength() is at most 31,
+        // so delta < 0 (see _truncateToN).
+        return msg;
+    }
+    assert(msg.length === 63);
+    // In this case delta will be 4 so we perform a shift-left of 4 bits by adding a ZERO_BN.
+    return `${msg}0`;
+}
+
+export function sign(keyPair: ec.KeyPair, msg: string): ec.Signature {
+    return keyPair.sign(fixMessage(msg));
+}
---
+++ src/stark/stark-key.ts
@@ -0,0 +1,80 @@
+import { getIntFromBits } from './stark-curve';
+import hashJS from 'hash.js';
+import { grindKey } from '../crypto/crypto';
+import { hdkey } from 'ethereumjs-wallet';
+import { curves, ec as Ec } from 'elliptic';
+import BN from 'bn.js';
+import { constantPointsHex } from './points';
+import * as encUtils from 'enc-utils';
+import { ec } from 'elliptic';
+
+const prime = new BN(
+    '800000000000011000000000000000000000000000000000000000000000001',
+    16,
+);
+const order = new BN(
+    '08000000 00000010 ffffffff ffffffff b781126d cae7b232 1e66a241 adc64d2f',
+    16,
+);
+
+export function getAccountPath(
+    layer: string,
+    application: string,
+    ethereumAddress: string,
+    index: string,
+): string {
+    const layerHash = hashJS.sha256().update(layer).digest('hex');
+    const applicationHash = hashJS.sha256().update(application).digest('hex');
+    const layerInt = getIntFromBits(layerHash, -31);
+    const applicationInt = getIntFromBits(applicationHash, -31);
+    const ethAddressInt1 = getIntFromBits(ethereumAddress, -31);
+    const ethAddressInt2 = getIntFromBits(ethereumAddress, -62, -31);
+    return `m/2645'/${layerInt}'/${applicationInt}'/${ethAddressInt1}'/${ethAddressInt2}'/${index}`;
+}
+
+export function getKeyPairFromPath(seed: string, path: string): ec.KeyPair {
+    const privateKey = hdkey
+        .fromMasterSeed(Buffer.from(seed.slice(2), 'hex')) // assuming seed is '0x...'
+        .derivePath(path)
+        .getWallet()
+        .getPrivateKeyString();
+    return getKeyPair(grindKey(privateKey));
+}
+
+const starkEc = new Ec(
+    new curves.PresetCurve({
+        type: 'short',
+        prime: null,
+        p: prime as any,
+        a: '00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000001',
+        b: '06f21413 efbe40de 150e596d 72f7a8c5 609ad26c 15c915c1 f4cdfcb9 9cee9e89',
+        n: order as any,
+        hash: hashJS.sha256,
+        gRed: false,
+        g: constantPointsHex[1],
+    }),
+);
+
+export function getKeyPair(privateKey: string): ec.KeyPair {
+    return starkEc.keyFromPrivate(privateKey, 'hex');
+}
+export function getPublic(keyPair: ec.KeyPair, compressed = false): string {
+    return keyPair.getPublic(compressed, 'hex');
+}
+
+export function getStarkPublicKey(keyPair: ec.KeyPair): string {
+    return getPublic(keyPair, true);
+}
+
+export function getKeyPairFromPublicKey(publicKey: string): ec.KeyPair {
+    return starkEc.keyFromPublic(encUtils.hexToArray(publicKey));
+}
+
+export function getKeyPairFromPrivateKey(privateKey: string): ec.KeyPair {
+    return starkEc.keyFromPrivate(privateKey, 'hex');
+}
+
+export function getXCoordinate(publicKey: string): string {
+    const keyPair = getKeyPairFromPublicKey(publicKey);
+    return encUtils.sanitizeBytes((keyPair as any).pub.getX().toString(16), 2);
+}
