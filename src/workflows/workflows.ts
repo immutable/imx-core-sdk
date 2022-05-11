a3c65172b57dbdf989dc388121f127d78836fe46
allan almeida <allan.almeida91@gmail.com>
2022-05-12 08:29:45 +1000

    add workflow

--- src/workflows/workflows.ts
+++ src/workflows/workflows.ts
@@ -1,15 +1,18 @@
-import { Configuration, MintsApi, UsersApi } from '../api';
+import { Configuration, DepositsApi, MintsApi, UsersApi } from '../api';
 import { Signer } from '@ethersproject/abstract-signer';
 import { registerOffchainWorkflow } from './registration';
 import { mintingWorkflow } from './minting';
 import { UnsignedMintRequest } from '../types';
+import { depositEthWorkflow } from './deposit';
 
 export class Workflows {
   private readonly usersApi: UsersApi;
   private readonly mintsApi: MintsApi;
+  private readonly depositsApi: DepositsApi;
   constructor(protected configuration: Configuration) {
     this.usersApi = new UsersApi(configuration);
     this.mintsApi = new MintsApi(configuration);
+    this.depositsApi = new DepositsApi(configuration);
   }
 
   public registerOffchain(signer: Signer) {
@@ -18,5 +21,9 @@ export class Workflows {
 
   public mint(signer: Signer, request: UnsignedMintRequest) {
     return mintingWorkflow(signer, request, this.mintsApi);
+  }
+
+  public depositEth(signer: Signer, amount: string) {
+    return depositEthWorkflow(signer, amount, this.depositsApi);
   }
 }
