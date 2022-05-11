f5ddf4450689a2d296b2b855e3d7b1e2af765d0d
Bruno Silva <brunosilva@192-168-1-101.tpgi.com.au>
2022-05-11 15:17:00 +1000

    Creates mint workflow

--- src/workflows/workflows.ts
+++ src/workflows/workflows.ts
@@ -1,14 +1,22 @@
-import { Configuration, UsersApi } from "../api";
+import {Configuration, MintsApi, UsersApi} from "../api";
 import { Signer } from "@ethersproject/abstract-signer";
 import { registerOffchainWorkflow } from "./registration";
+import { mintingWorkflow } from "./minting";
+import { UnsignedMintRequest } from "../types";
 
 export class Workflows {
     private readonly usersApi: UsersApi
+    private readonly mintsApi: MintsApi
     constructor(protected configuration: Configuration) {
         this.usersApi = new UsersApi(configuration)
+        this.mintsApi = new MintsApi(configuration)
     }
 
     public registerOffchain(signer: Signer) {
         return registerOffchainWorkflow(signer, this.usersApi)
+    }
+
+    public mint(signer: Signer, request: UnsignedMintRequest) {
+        return mintingWorkflow(signer, request, this.mintsApi)
     }
 }
