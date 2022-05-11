28005c489a44f4e2674b8440d218607904eeea24
Bruno Silva <100642763+bruno-imx@users.noreply.github.com>
2022-05-11 15:27:45 +1000

    Minting workflow (#16)
    * exports types
    
    * Creates mint workflow
    
    Co-authored-by: Bruno Silva <brunosilva@192-168-1-101.tpgi.com.au>

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,9 @@
 import { ec } from 'elliptic';
+import { MintRequest } from "../api";
 
 export interface StarkWallet {
     starkPublicKey: string;
     starkKeyPair: ec.KeyPair;
 }
+
+export type UnsignedMintRequest = Omit<MintRequest, "auth_signature">
