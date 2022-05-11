6c849268da98e44d9bc4a6906fbd41427f7c4a8f
allan almeida <allan.almeida91@gmail.com>
2022-05-12 07:21:03 +1000

    lint files

--- src/workflows/minting.ts
+++ src/workflows/minting.ts
@@ -1,63 +1,70 @@
-import {Signer} from "@ethersproject/abstract-signer";
-import {MintRequest, MintsApi, MintsApiMintTokensRequest, MintTokensResponse} from "../api";
-import {UnsignedMintRequest} from "../types";
-import {signRaw} from "../utils";
-import {keccak256} from '@ethersproject/keccak256';
-import {toUtf8Bytes} from '@ethersproject/strings';
+import { Signer } from '@ethersproject/abstract-signer';
+import {
+  MintRequest,
+  MintsApi,
+  MintsApiMintTokensRequest,
+  MintTokensResponse,
+} from '../api';
+import { UnsignedMintRequest } from '../types';
+import { signRaw } from '../utils';
+import { keccak256 } from '@ethersproject/keccak256';
+import { toUtf8Bytes } from '@ethersproject/strings';
 
-
-export async function mintingWorkflow(signer: Signer,
-                                      request: UnsignedMintRequest,
-                                      mintsApi: MintsApi,
-                                      ): Promise<MintTokensResponse> {
-
-
-    //TODO: improve this object key rearrangement.
-    //object keys should respect this order, but the logic can be improved
-    const users = request.users.map(user => ({
-        ether_key: user.user.toLowerCase(),
-        tokens: user.tokens.map(token => ({
-                id: token.id,
-                ...(token.blueprint && { blueprint: token.blueprint }),
-                ...(token.royalties && token.royalties.length > 0 && { royalties: token.royalties.map(royalty => ( {
-                        recipient: royalty.recipient.toLowerCase(),
-                        percentage: royalty.percentage,
-                    })) })
-            }))
-    }))
-
-    const royalties = request.royalties;
-    const signablePayload = {
-        contract_address: request.contract_address,
-        ...(royalties && royalties.length > 0 && {
-            royalties: royalties.map(fee => ({
-                recipient: fee.recipient.toLowerCase(),
-                percentage: fee.percentage,
-            })),
+export async function mintingWorkflow(
+  signer: Signer,
+  request: UnsignedMintRequest,
+  mintsApi: MintsApi,
+): Promise<MintTokensResponse> {
+  //TODO: improve this object key rearrangement.
+  //object keys should respect this order, but the logic can be improved
+  const users = request.users.map(user => ({
+    ether_key: user.user.toLowerCase(),
+    tokens: user.tokens.map(token => ({
+      id: token.id,
+      ...(token.blueprint && { blueprint: token.blueprint }),
+      ...(token.royalties &&
+        token.royalties.length > 0 && {
+          royalties: token.royalties.map(royalty => ({
+            recipient: royalty.recipient.toLowerCase(),
+            percentage: royalty.percentage,
+          })),
         }),
-        users,
-        auth_signature: '',
-    };
+    })),
+  }));
 
-    // Sign payload using L1 credentials
-    const hash = keccak256(toUtf8Bytes(JSON.stringify(signablePayload)));
-    const authSignature = await signRaw(hash, signer);
-
-    const apiPayload: MintRequest = {
-        users: signablePayload.users.map(user => ({
-            user: user.ether_key,
-            tokens: user.tokens,
+  const royalties = request.royalties;
+  const signablePayload = {
+    contract_address: request.contract_address,
+    ...(royalties &&
+      royalties.length > 0 && {
+        royalties: royalties.map(fee => ({
+          recipient: fee.recipient.toLowerCase(),
+          percentage: fee.percentage,
         })),
-        ...(royalties && royalties.length > 0 && { royalties }),
-        contract_address: request.contract_address,
-        auth_signature: authSignature,
-    };
+      }),
+    users,
+    auth_signature: '',
+  };
 
-    const apiRequest: MintsApiMintTokensRequest = {
-        mintTokensRequestV2: [apiPayload],
-    };
+  // Sign payload using L1 credentials
+  const hash = keccak256(toUtf8Bytes(JSON.stringify(signablePayload)));
+  const authSignature = await signRaw(hash, signer);
 
-    const response = await mintsApi.mintTokens(apiRequest);
+  const apiPayload: MintRequest = {
+    users: signablePayload.users.map(user => ({
+      user: user.ether_key,
+      tokens: user.tokens,
+    })),
+    ...(royalties && royalties.length > 0 && { royalties }),
+    contract_address: request.contract_address,
+    auth_signature: authSignature,
+  };
 
-    return response.data
+  const apiRequest: MintsApiMintTokensRequest = {
+    mintTokensRequestV2: [apiPayload],
+  };
+
+  const response = await mintsApi.mintTokens(apiRequest);
+
+  return response.data;
 }
--- src/workflows/workflows.ts
+++ src/workflows/workflows.ts
@@ -1,22 +1,22 @@
-import {Configuration, MintsApi, UsersApi} from "../api";
-import { Signer } from "@ethersproject/abstract-signer";
-import { registerOffchainWorkflow } from "./registration";
-import { mintingWorkflow } from "./minting";
-import { UnsignedMintRequest } from "../types";
+import { Configuration, MintsApi, UsersApi } from '../api';
+import { Signer } from '@ethersproject/abstract-signer';
+import { registerOffchainWorkflow } from './registration';
+import { mintingWorkflow } from './minting';
+import { UnsignedMintRequest } from '../types';
 
 export class Workflows {
-    private readonly usersApi: UsersApi
-    private readonly mintsApi: MintsApi
-    constructor(protected configuration: Configuration) {
-        this.usersApi = new UsersApi(configuration)
-        this.mintsApi = new MintsApi(configuration)
-    }
+  private readonly usersApi: UsersApi;
+  private readonly mintsApi: MintsApi;
+  constructor(protected configuration: Configuration) {
+    this.usersApi = new UsersApi(configuration);
+    this.mintsApi = new MintsApi(configuration);
+  }
 
-    public registerOffchain(signer: Signer) {
-        return registerOffchainWorkflow(signer, this.usersApi)
-    }
+  public registerOffchain(signer: Signer) {
+    return registerOffchainWorkflow(signer, this.usersApi);
+  }
 
-    public mint(signer: Signer, request: UnsignedMintRequest) {
-        return mintingWorkflow(signer, request, this.mintsApi)
-    }
+  public mint(signer: Signer, request: UnsignedMintRequest) {
+    return mintingWorkflow(signer, request, this.mintsApi);
+  }
 }
