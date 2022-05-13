eac018aba781de4a5c732b631a246c89d09d9df6
Allan Almeida <allan.almeida91@gmail.com>
2022-05-13 10:57:49 +1000

    [DXP-797] Support all deposit token types (#18)
    * refactor
    
    * reconfigure deposit eth workflowg
    
    * add support to deposit eth when user not registerd onchain
    
    * clean up
    
    * add seperate workflow
    
    * rename types
    
    * start deposit ERC20 workflow
    
    * add encoding api to deposit eth workflow
    
    * add encoding api to deposit erc20 workflow
    
    * use correct contract calls for erc20
    
    * add seperate workflow
    
    * add workflow for deposit nft
    
    * clean up
    
    * update changelog
    
    * clean up
    
    * fix rebase conflicts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,19 @@
 import { ec } from 'elliptic';
-import { MintRequest, GetSignableTransferRequestV1, GetSignableTransferRequest } from "../api";
+import {
+  MintRequest,
+  GetSignableTransferRequestV1,
+  GetSignableTransferRequest,
+} from '../api';
 import { GetSignableBurnRequest } from '../workflows/types';
 
 export interface StarkWallet {
-    starkPublicKey: string;
-    starkKeyPair: ec.KeyPair;
+  starkPublicKey: string;
+  starkKeyPair: ec.KeyPair;
 }
 
-export type UnsignedMintRequest = Omit<MintRequest, "auth_signature">
-export type UnsignedTransferRequest = GetSignableTransferRequestV1
-export type UnsignedBatchNftTransferRequest = GetSignableTransferRequest
-export type UnsignedBurnRequest = GetSignableBurnRequest
+export type UnsignedMintRequest = Omit<MintRequest, 'auth_signature'>;
+export type UnsignedTransferRequest = GetSignableTransferRequestV1;
+export type UnsignedBatchNftTransferRequest = GetSignableTransferRequest;
+export type UnsignedBurnRequest = GetSignableBurnRequest;
+
+export * from './deposit';
