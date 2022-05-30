d7af9551c366e12bd21d071a101bbbf11da9120c
Chris James <chris@webglowit.net>
2022-05-30 15:56:13 +1000

    fix: update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@typechain/hardhat';
-import '@nomiclabs/hardhat-ethers';
+// @ts-check
+require('@typechain/hardhat');
+require('@nomiclabs/hardhat-ethers');
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: '0.8.11',
   typechain: {
     outDir: 'src/contracts',
@@ -13,4 +13,4 @@ const config: HardhatUserConfig = {
   },
 };
 
-export default config;
+module.exports = config;
