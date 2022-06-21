4c82ee5368e2139cf7d309623254fce7f9e8cc1b
Alex Connolly <alexconnollyprivate@gmail.com>
2022-06-21 10:07:12 +1000

    Dependency Reduction (#75)
    * Remove initial unused
    
    * dependency updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import { DEFAULT_EXTENSIONS } from '@babel/core';
 import babel from '@rollup/plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import { externals } from 'rollup-plugin-node-externals';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 
@@ -41,21 +42,14 @@ export default [
         exports: 'named',
       },
     ],
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ],
     plugins: [
+      externals(),
       json(),
       resolve(),
       typescript({
         tsconfig: './tsconfig.json',
       }),
-      commonjs({
-        namedExports: {
-          elliptic: ['ec', 'curves'],
-        },
-      }),
+      commonjs(),
       babel({
         presets: [],
         extensions: [...DEFAULT_EXTENSIONS, '.ts'],
