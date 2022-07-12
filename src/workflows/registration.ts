ce295de8f5ee1ccc3dd46e31cc0c6a9bfab2d922
Victor Miguez <victor.miguez@immutable.com>
2022-07-12 15:23:39 +1000

    Checking if user is registered on registerOffchainWorkflow (#85)
    * Checking if user is registered on registerOffchainWorkflow
    
    * Ignoring errors from getUsers api
    
    * Updating isUserRegistered function to return false in case of any errors
    
    * Reflecting changes on the changelog

--- src/workflows/registration.ts
+++ src/workflows/registration.ts
@@ -54,9 +54,13 @@ export async function registerOffchainWorkflowWithSigner(
   l1Signer: Signer,
   l2Signer: L2Signer,
   usersApi: UsersApi,
-): Promise<RegisterUserResponse> {
+): Promise<void> {
   const userAddress = await l1Signer.getAddress();
-  const starkPublicKey = await l2Signer.getAddress();
+  const starkPublicKey = l2Signer.getAddress();
+
+  if (await isUserRegistered(userAddress, usersApi)) {
+    return;
+  }
 
   // Get signable details for offchain registration
   const signableResult = await usersApi.getSignableRegistrationOffchain({
@@ -76,7 +80,7 @@ export async function registerOffchainWorkflowWithSigner(
   const starkSignature = await l2Signer.signMessage(payloadHash);
 
   // Send request for user registration offchain
-  const response = await usersApi.registerUser({
+  await usersApi.registerUser({
     registerUserRequest: {
       eth_signature: ethSignature,
       ether_key: userAddress,
@@ -85,9 +89,7 @@ export async function registerOffchainWorkflowWithSigner(
     },
   });
 
-  return {
-    tx_hash: response.data.tx_hash,
-  };
+  return;
 }
 
 export async function isRegisteredOnChainWorkflow(
@@ -112,4 +114,16 @@ export async function getSignableRegistrationOnchain(
     operator_signature: response.data.operator_signature,
     payload_hash: response.data.payload_hash,
   };
+}
+
+async function isUserRegistered(
+  userAddress: string,
+  usersApi: UsersApi,
+): Promise<boolean> {
+  try {
+    await usersApi.getUsers({ user: userAddress });
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
