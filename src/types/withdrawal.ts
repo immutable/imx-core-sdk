72569f55fb31b5c2af9b85281ef25cac3608c881
yundifu <yundi.fu77@gmail.com>
2022-07-13 13:41:25 +1000

    wip:add empty line

--- src/types/withdrawal.ts
+++ src/types/withdrawal.ts
@@ -48,7 +48,7 @@ export interface ERC20PrepareWithdrawal {
 export type TokenWithdrawal = ETHWithdrawal | ERC20Withdrawal | ERC721Withdrawal;
 export type TokenPrepareWithdrawal = ETHPrepareWithdrawal | ERC20PrepareWithdrawal | ERC721Withdrawal;
 
-export type PrepareWithdrawalRequest = {
+export type PrepareWithdrawalRequest = WalletConnection & {
   token: TokenPrepareWithdrawal;
   quantity: string;
-} & WalletConnection
+}
