77a28367dd35e6e90dd20aa647a2f73765dae646
Nik Ho <studying.mole@gmail.com>
2022-08-15 19:07:26 +1200

    Add workflow methods to IMX client class.

--- src/IMXClientClass.ts
+++ src/IMXClientClass.ts
@@ -1,3 +1,4 @@
+import { Signer } from '@ethersproject/abstract-signer';
 import {
   AssetsApi,
   BalancesApi,
@@ -12,8 +13,23 @@ import {
   TransfersApi,
   UsersApi,
   WithdrawalsApi,
+  MintRequest,
+  GetSignableTransferRequestV1,
+  GetSignableTransferRequest,
+  GetSignableOrderRequest,
+  GetSignableCancelOrderRequest,
+  GetSignableTradeRequest,
+  // Configuration as APIConfiguration,
 } from './api';
-import { ImmutableXConfiguration, WalletConnection } from './types';
+import {
+  ImmutableXConfiguration,
+  WalletConnection,
+  UnsignedMintRequest,
+  PrepareWithdrawalRequest,
+  TokenDeposit,
+  TokenWithdrawal,
+} from './types';
+import { GetSignableBurnRequest } from './workflows/types';
 import { Workflows } from './workflows';
 import {
   createStarkSigner,
@@ -22,7 +38,7 @@ import {
   getKeyPair,
 } from './exportutils';
 
-export * from './types';
+// export * from './types';
 
 export class ImxClient {
   private depositsApi;
@@ -57,104 +73,186 @@ export class ImxClient {
     this.workflows = new Workflows(config);
   }
 
+  /**
+   * Workflow Methods
+   */
   public registerOffchain(walletConnection: WalletConnection) {
     return this.workflows.registerOffchain(walletConnection);
   }
+
+  public isRegisteredOnchain(walletConnection: WalletConnection) {
+    return this.workflows.isRegisteredOnchain(walletConnection);
+  }
+
+  public mint(signer: Signer, request: UnsignedMintRequest) {
+    return this.workflows.mint(signer, request);
+  }
+
+  public burn(
+    walletConnection: WalletConnection,
+    request: GetSignableBurnRequest,
+  ) {
+    return this.workflows.burn(walletConnection, request);
+  }
+
+  public transfer(
+    walletConnection: WalletConnection,
+    request: GetSignableTransferRequestV1,
+  ) {
+    return this.workflows.transfer(walletConnection, request);
+  }
+
+  public batchNftTransfer(
+    walletConnection: WalletConnection,
+    request: GetSignableTransferRequest,
+  ) {
+    return this.workflows.batchNftTransfer(walletConnection, request);
+  }
+
+  public prepareWithdrawal(
+    walletConnection: WalletConnection,
+    request: PrepareWithdrawalRequest,
+  ) {
+    return this.workflows.prepareWithdrawal(walletConnection, request);
+  }
+
+  public deposit(signer: Signer, deposit: TokenDeposit) {
+    return this.workflows.deposit(signer, deposit);
+  }
+
+  public completeWithdrawal(
+    signer: Signer,
+    starkPublicKey: string,
+    token: TokenWithdrawal,
+  ) {
+    return this.workflows.completeWithdrawal(signer, starkPublicKey, token);
+  }
+
+  public createOrder(
+    walletConnection: WalletConnection,
+    request: GetSignableOrderRequest,
+  ) {
+    return this.workflows.createOrder(walletConnection, request);
+  }
+  public cancelOrder(
+    walletConnection: WalletConnection,
+    request: GetSignableCancelOrderRequest,
+  ) {
+    return this.workflows.cancelOrder(walletConnection, request);
+  }
+
+  public createTrade(
+    walletConnection: WalletConnection,
+    request: GetSignableTradeRequest,
+  ) {
+    return this.workflows.createTrade(walletConnection, request);
+  }
+
+  /**
+   * Util Methods
+   */
+  // TODO
+
+  /**
+   * API Methods
+   */
+  // TODO
+
 }
 
-export const IMXClientFactory = (config: ImmutableXConfiguration) => {
-  const depositsApi = new DepositsApi(config.apiConfiguration);
-  const mintsApi = new MintsApi(config.apiConfiguration);
-  const ordersApi = new OrdersApi(config.apiConfiguration);
-  const tokensApi = new TokensApi(config.apiConfiguration);
-  const tradesApi = new TradesApi(config.apiConfiguration);
-  const transfersApi = new TransfersApi(config.apiConfiguration);
-  const usersApi = new UsersApi(config.apiConfiguration);
-  const withdrawalsApi = new WithdrawalsApi(config.apiConfiguration);
-  const balanceApi = new BalancesApi(config.apiConfiguration);
-  const assetApi = new AssetsApi(config.apiConfiguration);
-  const collectionApi = new CollectionsApi(config.apiConfiguration);
-  const metadataApi = new MetadataApi(config.apiConfiguration);
-  const projectsApi = new ProjectsApi(config.apiConfiguration);
-  const workflows = new Workflows(config);
+// export const IMXClientFactory = (config: ImmutableXConfiguration) => {
+//   const depositsApi = new DepositsApi(config.apiConfiguration);
+//   const mintsApi = new MintsApi(config.apiConfiguration);
+//   const ordersApi = new OrdersApi(config.apiConfiguration);
+//   const tokensApi = new TokensApi(config.apiConfiguration);
+//   const tradesApi = new TradesApi(config.apiConfiguration);
+//   const transfersApi = new TransfersApi(config.apiConfiguration);
+//   const usersApi = new UsersApi(config.apiConfiguration);
+//   const withdrawalsApi = new WithdrawalsApi(config.apiConfiguration);
+//   const balanceApi = new BalancesApi(config.apiConfiguration);
+//   const assetApi = new AssetsApi(config.apiConfiguration);
+//   const collectionApi = new CollectionsApi(config.apiConfiguration);
+//   const metadataApi = new MetadataApi(config.apiConfiguration);
+//   const projectsApi = new ProjectsApi(config.apiConfiguration);
+//   const workflows = new Workflows(config);
 
-  // Client Interface
-  return {
-    // Workflows
-    registerOffchain: workflows.registerOffchain,
-    isRegisteredOnchain: workflows.isRegisteredOnchain,
-    mint: workflows.mint,
-    burn: workflows.burn,
-    transfer: workflows.transfer,
-    batchNftTransfer: workflows.batchNftTransfer,
-    prepareWithdrawal: workflows.prepareWithdrawal,
-    deposit: workflows.deposit,
-    completeWithdrawal: workflows.completeWithdrawal,
-    createOrder: workflows.createOrder,
-    cancelOrder: workflows.cancelOrder,
-    createTrade: workflows.createTrade,
+//   // Client Interface
+//   return {
+//     // Workflows
+//     registerOffchain: workflows.registerOffchain,
+//     isRegisteredOnchain: workflows.isRegisteredOnchain,
+//     mint: workflows.mint,
+//     burn: workflows.burn,
+//     transfer: workflows.transfer,
+//     batchNftTransfer: workflows.batchNftTransfer,
+//     prepareWithdrawal: workflows.prepareWithdrawal,
+//     deposit: workflows.deposit,
+//     completeWithdrawal: workflows.completeWithdrawal,
+//     createOrder: workflows.createOrder,
+//     cancelOrder: workflows.cancelOrder,
+//     createTrade: workflows.createTrade,
 
-    // Utils
-    createStarkSigner,
-    generateStarkKeyPair,
-    generateStarkKeyPairFromSignedMessage,
-    getKeyPair,
+//     // Utils
+//     createStarkSigner,
+//     generateStarkKeyPair,
+//     generateStarkKeyPairFromSignedMessage,
+//     getKeyPair,
 
-    // API calls
-    // Assets
-    getAsset: assetApi.getAsset,
-    listAsset: assetApi.listAssets,
+//     // API calls
+//     // Assets
+//     getAsset: assetApi.getAsset,
+//     listAsset: assetApi.listAssets,
 
-    // Collections
-    createCollection: collectionApi.createCollection,
-    getCollection: collectionApi.getCollection,
-    listCollectionFilters: collectionApi.listCollectionFilters,
-    listCollections: collectionApi.listCollections,
-    updateCollection: collectionApi.updateCollection,
+//     // Collections
+//     createCollection: collectionApi.createCollection,
+//     getCollection: collectionApi.getCollection,
+//     listCollectionFilters: collectionApi.listCollectionFilters,
+//     listCollections: collectionApi.listCollections,
+//     updateCollection: collectionApi.updateCollection,
 
-    // Metadata
-    addMetadataSchemaToCollection: metadataApi.addMetadataSchemaToCollection,
-    getMetadataSchema: metadataApi.getMetadataSchema,
-    updateMetadataSchemaByName: metadataApi.updateMetadataSchemaByName,
+//     // Metadata
+//     addMetadataSchemaToCollection: metadataApi.addMetadataSchemaToCollection,
+//     getMetadataSchema: metadataApi.getMetadataSchema,
+//     updateMetadataSchemaByName: metadataApi.updateMetadataSchemaByName,
 
-    // Projects
-    createProject: projectsApi.createProject,
-    getProject: projectsApi.getProject,
-    getProjects: projectsApi.getProjects,
+//     // Projects
+//     createProject: projectsApi.createProject,
+//     getProject: projectsApi.getProject,
+//     getProjects: projectsApi.getProjects,
 
-    // Balance
-    getBalance: balanceApi.getBalance,
-    listBalances: balanceApi.listBalances,
+//     // Balance
+//     getBalance: balanceApi.getBalance,
+//     listBalances: balanceApi.listBalances,
 
-    // Deposits
-    getDeposit: depositsApi.getDeposit,
-    listDeposits: depositsApi.listDeposits,
+//     // Deposits
+//     getDeposit: depositsApi.getDeposit,
+//     listDeposits: depositsApi.listDeposits,
 
-    // MintsApi
-    getMint: mintsApi.getMint,
-    listMints: mintsApi.listMints,
+//     // MintsApi
+//     getMint: mintsApi.getMint,
+//     listMints: mintsApi.listMints,
 
-    // Withdrawal
-    listWithdrawals: withdrawalsApi.listWithdrawals,
-    getWithdrawal: withdrawalsApi.getWithdrawal,
+//     // Withdrawal
+//     listWithdrawals: withdrawalsApi.listWithdrawals,
+//     getWithdrawal: withdrawalsApi.getWithdrawal,
 
-    // Users
-    getUsers: usersApi.getUsers,
+//     // Users
+//     getUsers: usersApi.getUsers,
 
-    // Order
-    getOrder: ordersApi.getOrder,
-    listOrders: ordersApi.listOrders,
+//     // Order
+//     getOrder: ordersApi.getOrder,
+//     listOrders: ordersApi.listOrders,
 
-    // Trades
-    getTrade: tradesApi.getTrade,
-    listTrades: tradesApi.listTrades,
+//     // Trades
+//     getTrade: tradesApi.getTrade,
+//     listTrades: tradesApi.listTrades,
 
-    // Tokens
-    getToken: tokensApi.getToken,
-    listTokens: tokensApi.listTokens,
+//     // Tokens
+//     getToken: tokensApi.getToken,
+//     listTokens: tokensApi.listTokens,
 
-    // Transfers
-    getTransfer: transfersApi.getTransfer,
-    listTransfers: transfersApi.listTransfers,
-  };
-};
+//     // Transfers
+//     getTransfer: transfersApi.getTransfer,
+//     listTransfers: transfersApi.listTransfers,
+//   };
+// };
