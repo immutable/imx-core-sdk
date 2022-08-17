bd378dab94ff3a4461a841792f73041c73744649
Nik Ho <studying.mole@gmail.com>
2022-08-18 11:43:48 +1200

    wip - test IMX class, build docs

--- sdk-tester/index.ts
+++ sdk-tester/index.ts
@@ -1,17 +1,17 @@
-import { Utils, Apis, IMXClientClass, IMXClientFactory } from '@imtbl/core-sdk';
+import { Utils, IMXClientClass } from '@imtbl/core-sdk';
 
 (async () => {
   const config = Utils.Config.sandbox;
 
   const classClient = new IMXClientClass(config);
-  const factoryClient = IMXClientFactory(config);
+  // const factoryClient = IMXClientFactory(config);
   
   // API classes
-  const collectionsApi = new Apis.CollectionsApi();
-  const collections1 = await collectionsApi.listCollections();
-  console.group('Collections 1');
-  console.log(collections1.data.result.slice(0,5));
-  console.groupEnd();
+  // const collectionsApi = new Apis.CollectionsApi();
+  // const collections1 = await collectionsApi.listCollections();
+  // console.group('Collections 1');
+  // console.log(collections1.data.result.slice(0,5));
+  // console.groupEnd();
   
   // IMX class client
   const collections2 = await classClient.listCollections();
@@ -20,8 +20,8 @@ import { Utils, Apis, IMXClientClass, IMXClientFactory } from '@imtbl/core-sdk';
   console.groupEnd();
 
   // IMX factory client
-  const collections3 = await factoryClient.listCollections();
-  console.group('Collections 3');
-  console.log(collections3.data.result.slice(0,5));
-  console.groupEnd();
+  // const collections3 = await factoryClient.listCollections();
+  // console.group('Collections 3');
+  // console.log(collections3.data.result.slice(0,5));
+  // console.groupEnd();
 })();
