5cf576dae8f4582cc6206d337b497391d44371fe
Nik Ho <studying.mole@gmail.com>
2022-08-17 14:01:27 +1200

    Export all types, remove src folder from package output.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * as Contracts from './exportcontracts';
 export * as Utils from './exportutils';
 export * from './workflows';
 export * from './types';
+export * from './api';
 export { IMXClientFactory } from './IMXClientFactory';
 export { IMXClientClass } from './IMXClientClass';
