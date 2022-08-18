fce9f856d88cccd85c52ef07358df062d9d5d466
Nik Ho <studying.mole@gmail.com>
2022-08-18 15:01:14 +1200

    wip - build factory client docs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export * as Apis from './exportapis';
+// export * as Apis from './exportapis';
 export * as Contracts from './exportcontracts';
 export * as Utils from './exportutils';
 export * from './workflows';
 export * from './types';
 export * from './api';
 export { IMXClientFactory } from './IMXClientFactory';
-export { IMXClientClass } from './IMXClientClass';
+// export { IMXClientClass } from './IMXClientClass';
