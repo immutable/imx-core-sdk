fcf404412f59e498b1a8e370a87e560ec5c26dba
Kostia Kim <kostia.kim@immutable.com>
2022-09-09 08:50:57 +1000

    move code

--- src/utils/stark/stark-signer.ts
+++ src/utils/stark/stark-signer.ts
@@ -6,22 +6,6 @@ import { getStarkPublicKey } from './stark-curve';
 import BN from 'bn.js';
 import { Errors } from '../../workflows/errors';
 
-function fixMessage(msg: string) {
-  msg = encUtils.removeHexPrefix(msg);
-  msg = new BN(msg, 16).toString(16);
-
-  if (msg.length <= 62) {
-    // In this case, msg should not be transformed, as the byteLength() is at most 31,
-    // so delta < 0 (see _truncateToN).
-    return msg;
-  }
-  if (msg.length !== 63) {
-    throw new Error(Errors.StarkCurveInvalidMessageLength);
-  }
-  // In this case delta will be 4 so we perform a shift-left of 4 bits by adding a ZERO_BN.
-  return `${msg}0`;
-}
-
 class StandardStarkSigner implements StarkSigner {
   constructor(private keyPair: ec.KeyPair) {}
 
@@ -30,7 +14,7 @@ class StandardStarkSigner implements StarkSigner {
   }
 
   public async signMessage(msg: string): Promise<string> {
-    return this.serialize(this.keyPair.sign(fixMessage(msg)));
+    return this.serialize(this.keyPair.sign(this.fixMessage(msg)));
   }
 
   private serialize(sig: ec.Signature): string {
@@ -38,6 +22,22 @@ class StandardStarkSigner implements StarkSigner {
       encUtils.padLeft(sig.r.toString(16), 64) +
         encUtils.padLeft(sig.s.toString(16), 64),
     );
+  }
+
+  private fixMessage(msg: string) {
+    msg = encUtils.removeHexPrefix(msg);
+    msg = new BN(msg, 16).toString(16);
+
+    if (msg.length <= 62) {
+      // In this case, msg should not be transformed, as the byteLength() is at most 31,
+      // so delta < 0 (see _truncateToN).
+      return msg;
+    }
+    if (msg.length !== 63) {
+      throw new Error(Errors.StarkCurveInvalidMessageLength);
+    }
+    // In this case delta will be 4 so we perform a shift-left of 4 bits by adding a ZERO_BN.
+    return `${msg}0`;
   }
 }
 
