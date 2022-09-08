59d9060d3dc004a29245d60eee9301c6f60fe586
Kostia Kim <kostia.kim@immutable.com>
2022-09-08 18:06:53 +1000

    stark signer test

---
+++ src/utils/stark/stark-signer.test.ts
@@ -0,0 +1,14 @@
+import { createStarkSigner } from './stark-signer';
+
+describe('Stark signer', () => {
+  it('should return correct address', async () => {
+    const arbitraryPrivateKey =
+      '019b5de47e5fc8f2e8c3415b42a126aadb462637f2feca1df3733fe3f37cf50f';
+    const expectedPublicKey =
+      '0x0790436373c1d5b7a88ce4fd7ac96591a385b2b6392d1ea44a165f75115b82ac';
+
+    const signer = createStarkSigner(arbitraryPrivateKey);
+
+    expect(signer.getAddress()).toEqual(expectedPublicKey);
+  });
+});
