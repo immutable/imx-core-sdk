f10d5a2acaa79db95f918c23e521998c4bc9ff5f
Kostia Kim <kostia.kim@immutable.com>
2022-09-12 18:37:42 +1000

    sign message test

--- src/utils/stark/stark-signer.test.ts
+++ src/utils/stark/stark-signer.test.ts
@@ -11,4 +11,18 @@ describe('StarkSigner', () => {
 
     expect(signer.getAddress()).toEqual(expectedPublicKey);
   });
+
+  it('should sign message and produce expected signature', async () => {
+    const privateKey =
+      '7CEFD165C3A374AC3E05170D699BF6549E371522883B447B284A3C16FC04CCC';
+    const encodedMessage =
+      'e2919c6f19f93d3b9e40c1eef10660bd12240a1520793a28ef21a7457037dd';
+    const expectedSignature =
+      '0x0752063caed87ef11d6e91c4a226ebfe98f190d248b857d882ae331771e6e4620364a2c46e2190bbb243309a40da051b88f0657ea9d1c2ca11510fe18a8a22ae';
+    const signer = createStarkSigner(privateKey);
+
+    const signature = await signer.signMessage(encodedMessage);
+
+    expect(signature).toEqual(expectedSignature);
+  });
 });
