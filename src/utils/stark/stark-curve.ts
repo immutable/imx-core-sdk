b158fd26efff799c36e8e9ef12d6df3673117b9f
Kostia Kim <kostia.kim@immutable.com>
2022-09-15 13:40:57 +1000

    fix and test grindKey

--- src/utils/stark/stark-curve.ts
+++ src/utils/stark/stark-curve.ts
@@ -1,4 +1,4 @@
-import hashJS from 'hash.js';
+import hash from 'hash.js';
 import { curves, ec } from 'elliptic';
 import * as encUtils from 'enc-utils';
 import BN from 'bn.js';
@@ -27,6 +27,11 @@ G = (874739451078007766457464989774322083649278607533249481151382481072868806602
 https://docs.starkware.co/starkex-v4/crypto/stark-curve
 */
 
+export const starkEcOrder = new BN(
+  '08000000 00000010 ffffffff ffffffff b781126d cae7b232 1e66a241 adc64d2f',
+  16,
+);
+
 export const starkEc = new ec(
   new curves.PresetCurve({
     type: 'short',
@@ -34,8 +39,8 @@ export const starkEc = new ec(
     p: '08000000 00000011 00000000 00000000 00000000 00000000 00000000 00000001',
     a: '00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000001',
     b: '06f21413 efbe40de 150e596d 72f7a8c5 609ad26c 15c915c1 f4cdfcb9 9cee9e89',
-    n: '08000000 00000010 ffffffff ffffffff b781126d cae7b232 1e66a241 adc64d2f',
-    hash: hashJS.sha256,
+    n: starkEcOrder.toString('hex'),
+    hash: hash.sha256,
     gRed: false,
     g: [
       '1ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca',
@@ -60,7 +65,17 @@ function hashKeyWithIndex(key: string, index: number): BN {
   );
 }
 
-function grindKey(key: BN, keyValLimit: BN) {
+/*
+ This function receives a key seed and produces an appropriate StarkEx key from a uniform
+ distribution.
+ Although it is possible to define a StarkEx key as a residue between the StarkEx EC order and a
+ random 256bit digest value, the result would be a biased key. In order to prevent this bias, we
+ deterministically search (by applying more hashes, AKA grinding) for a value lower than the largest
+ 256bit multiple of StarkEx EC order.
+
+ https://github.com/starkware-libs/starkware-crypto-utils/blob/dev/src/js/key_derivation.js#L119
+*/
+export function grindKey(keySeed: BN, keyValLimit: BN) {
   const sha256EcMaxDigest = new BN(
     '1 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000',
     16,
@@ -68,9 +83,10 @@ function grindKey(key: BN, keyValLimit: BN) {
   const maxAllowedVal = sha256EcMaxDigest.sub(
     sha256EcMaxDigest.mod(keyValLimit),
   );
-  for (let i = 0; !key.lt(maxAllowedVal); i++) {
-    // Make sure the produced key is devided by the Stark EC order, and falls within the range
-    // [0, maxAllowedVal).
+  let key = hashKeyWithIndex(keySeed.toString('hex'), 0);
+  // Make sure the produced key is devided by the Stark EC order, and falls within the range
+  // [0, maxAllowedVal).
+  for (let i = 1; key.gte(maxAllowedVal); i++) {
     key = hashKeyWithIndex(key.toString('hex'), i);
   }
   return key.umod(keyValLimit).toString('hex');
@@ -79,5 +95,5 @@ function grindKey(key: BN, keyValLimit: BN) {
 // Generates a new private key on the Stark-friendly elliptic curve.
 export function generateStarkPrivateKey(): string {
   const keyPair = starkEc.genKeyPair();
-  return grindKey(keyPair.getPrivate(), starkEc.n!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
+  return grindKey(keyPair.getPrivate(), starkEcOrder);
 }
