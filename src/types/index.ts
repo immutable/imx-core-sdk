cec9e95392f0e08779d95a5eacb8586a7df4b88b
Matthew Muscat <officialmatthewmuscat@gmail.com>
2022-09-20 13:18:34 +1000

    feat: Add typedoc annotations to custom types & refactor export structure

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,32 +1,4 @@
-import { MintRequest, Configuration as APIConfiguration } from '../api';
-import { Signer as EthSigner } from '@ethersproject/abstract-signer';
-
-export { EthSigner };
-
-export interface StarkSigner {
-  signMessage(message: string): Promise<string>;
-  getAddress(): string | Promise<string>;
-}
-
-export type L2Signer = StarkSigner;
-
-export interface WalletConnection {
-  ethSigner: EthSigner;
-  starkSigner: StarkSigner;
-}
-
-export interface EthConfiguration {
-  coreContractAddress: string;
-  registrationContractAddress: string;
-  chainID: number;
-}
-
-export interface ImmutableXConfiguration {
-  apiConfiguration: APIConfiguration;
-  ethConfiguration: EthConfiguration;
-}
-
-export type UnsignedMintRequest = Omit<MintRequest, 'auth_signature'>;
-
+export * from './generic';
 export * from './tokens';
-export * from './custom-interfaces';
+export * from './requests';
+export * from './transfers';
--- src/types/tokens.ts
+++ src/types/tokens.ts
@@ -1,33 +1,64 @@
 import { SignableToken } from '../api';
 
+/**
+ * An ERC20 token
+ */
 export interface ERC20Token {
   type: 'ERC20';
   tokenAddress: string;
 }
 
+/**
+ * An ERC721 token
+ */
 export interface ERC721Token {
   type: 'ERC721';
   tokenId: string;
   tokenAddress: string;
 }
 
+/**
+ * An ETH token
+ */
 export interface ETHToken {
   type: 'ETH';
 }
 
+/**
+ * An amount of ETH token of unit Wei
+ */
 export interface ETHAmount extends ETHToken {
+  /**
+   * An amount in unit Wei
+   */
   amount: string;
 }
 
+/**
+ * The token details and amount of ERC20 token units
+ */
 export interface ERC20Amount extends ERC20Token {
+  /**
+   * An amount in ERC20 unit
+   */
   amount: string;
 }
 
+/**
+ * Union type that represents all token types
+ */
 export type AnyToken = ETHToken | ERC721Token | ERC20Token;
 
+/**
+ * Union type that represents all token type amounts
+ */
 export type TokenAmount = ETHAmount | ERC20Amount | ERC721Token;
 
-//SignableToken on the API requires fields in snake_case format
+/**
+ * Helper method to convert token type to a SignableToken type
+ * @param token - the token type to convert to a SignableToken type
+ * @returns the converted SignableToken
+ */
 export function convertToSignableToken(token: TokenAmount): SignableToken {
   switch (token.type) {
     case 'ERC721':
@@ -54,4 +85,3 @@ export function convertToSignableToken(token: TokenAmount): SignableToken {
       };
   }
 }
-
