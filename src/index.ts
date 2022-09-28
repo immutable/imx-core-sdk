5a30b223f8651d057d28cde506d6b64bdea26fdc
Aung T. T. Kyaw <zatlite@gmail.com>
2022-09-28 13:24:42 +1000

    chore: change file name to exportApiModels

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * as Contracts from './exportContracts';
 export * from './exportUtils';
-export * from './exportApis';
+export * from './exportApiModels';
 export * from './types';
 export { Config } from './config';
 export { ImmutableX } from './ImmutableX';
