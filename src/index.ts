8619bd20ce4e7623c7fe4d328652a9f72d650dee
Aung T. T. Kyaw <zatlite@gmail.com>
2022-09-28 12:29:36 +1000

    feat: only expose request and responses from api
    [DX-1251]

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * as Contracts from './exportContracts';
 export * from './exportUtils';
+export * from './exportApis';
 export * from './types';
-export * from './api';
 export { Config } from './config';
 export { ImmutableX } from './ImmutableX';
