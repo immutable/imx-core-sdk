79bb99bc512ba3301197087096af04ec47cde292
Aung T. T. Kyaw <zatlite@gmail.com>
2022-11-14 16:16:20 +1100

    chore: add extra test

--- src/utils/formatError.test.ts
+++ src/utils/formatError.test.ts
@@ -3,6 +3,16 @@ import { Config, ImmutableXConfiguration } from '../config';
 import { ImmutableX } from '../ImmutableX';
 
 describe('formatError', () => {
+  it('should format api errors to IMXError', async () => {
+    const client = new ImmutableX(Config.SANDBOX);
+    await expect(
+      client.getAsset({
+        tokenAddress: '0',
+        tokenId: '0',
+      }),
+    ).rejects.toThrowError("Asset with address 0 and id '0' not found");
+  });
+
   it('should format axios errors to IMXError', async () => {
     const client = new ImmutableX(Config.SANDBOX);
     await expect(client.getUser('')).rejects.toThrowError(
