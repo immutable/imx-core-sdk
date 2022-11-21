e9467450b3d9f7aed71a9600cc3c297f856f18d6
Felipe Tavares <felipe.tavares@pragma.team>
2022-11-21 11:37:58 +1100

    proper variable naming and logging @ example

--- examples/createNftSecondaryTransaction.ts
+++ examples/createNftSecondaryTransaction.ts
@@ -4,7 +4,7 @@ import { ImmutableX, Config } from '@imtbl/core-sdk';
     const imxClient = new ImmutableX(Config.SANDBOX);
 
     try {
-        const exchangeTransferResponse = await imxClient.createNftSecondaryTransaction({
+        const nftSecondaryTransaction = await imxClient.createNftSecondaryTransaction({
             createAPIRequest: {
                 "provider": "moonpay",
                 "user_wallet_address": "0x...",
@@ -12,7 +12,7 @@ import { ImmutableX, Config } from '@imtbl/core-sdk';
             }
         });
 
-        console.log('exchangeTransferResponse', exchangeTransferResponse);
+        console.log('nftSecondaryTransaction', nftSecondaryTransaction);
     } catch (error) {
         console.error(error);
         process.exit(1);
