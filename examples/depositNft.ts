108b54bf43c18a22031488b2c38ba97e30138c71
Jie Hui Tan <tanjiehui@Jies-MacBook-Pro.local>
2023-01-11 12:49:06 +0900

    Add deposit NFT example

---
+++ examples/depositNft.ts
@@ -0,0 +1,23 @@
+import { Config, ImmutableX } from '@imtbl/core-sdk';
+import { generateWalletConnection } from './libs/walletConnection';
+
+(async () => {
+  try {
+    const walletConnection = await generateWalletConnection('goerli');
+
+    // IMX class client
+    const client = new ImmutableX(Config.SANDBOX);
+
+    // Deposit ERC721
+    const depositResponse = await client.deposit(walletConnection.ethSigner, {
+      type: 'ERC721',
+      tokenId: '', // TOKEN_ID
+      tokenAddress: '' // COLLECTION_CONTRACT_ADDRESS
+    });
+
+    console.log('depositResponse', depositResponse);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
