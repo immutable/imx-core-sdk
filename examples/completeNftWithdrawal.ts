73c1cf21dcfba80735698091d09a766ae1ba0679
cdrabner95 <chris.drabner@immutable.com>
2023-01-25 13:48:35 -0500

    get user and withdrawal changes

---
+++ examples/completeNftWithdrawal.ts
@@ -0,0 +1,29 @@
+import { Config, ImmutableX } from '@imtbl/core-sdk';
+import { generateWalletConnection } from './libs/walletConnection';
+
+(async () => {
+  try {
+    const walletConnection = await generateWalletConnection('goerli');
+
+    const client = new ImmutableX(Config.SANDBOX);
+
+    const starkPublicKey = await walletConnection.starkSigner.getAddress();
+
+
+
+    const completeWithdrawalResponse = await client.completeWithdrawal(
+      walletConnection.ethSigner,
+      starkPublicKey,
+      {
+        type: 'ERC721',
+        tokenAddress: '',
+        tokenId: ''
+      },
+    );
+
+    console.log('completeWithdrawalResponse', completeWithdrawalResponse);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
