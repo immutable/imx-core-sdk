62c932058226f0cb380a18e47c8f5424b50fd760
cdrabner95 <chris.drabner@immutable.com>
2023-01-25 13:45:22 -0500

    Added project, transfers, and withdrawal examples

---
+++ examples/createErc721Withdrawal.ts
@@ -0,0 +1,26 @@
+import { Config, ImmutableX, TokenAmount } from '@imtbl/core-sdk';
+import { generateWalletConnection } from './libs/walletConnection';
+
+(async () => {
+  try {
+    const walletConnection = await generateWalletConnection('goerli');
+
+    const client = new ImmutableX(Config.SANDBOX);
+
+    const tokenAmount: TokenAmount = {
+      type: 'ERC721',
+      tokenAddress: "" , // contract address of token
+      tokenId: "" // token ID
+    };
+
+    const createWithdrawalResponse = await client.prepareWithdrawal(
+      walletConnection,
+      tokenAmount,
+    );
+
+    console.log('WithdrawalResponse', createWithdrawalResponse);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
