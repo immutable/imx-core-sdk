f569528579c48c35009069219ef1179f3742522a
Praveen Suluvai <praveen.suluvai@immutable.com>
2023-02-01 15:26:35 +1000

    lint fixes

--- examples/getProjects.ts
+++ examples/getProjects.ts
@@ -6,9 +6,6 @@ import { generateWalletConnection } from './libs/walletConnection';
     const client = new ImmutableX(Config.SANDBOX);
     const wallet = await generateWalletConnection('goerli');
     const pageSize = 200;
-    const cursor = ''; // cursor
-    const orderBy = ''; // order by created_at, updated_at
-    const direction = ''; //asc/desc
     const projects = await client.getProjects(wallet.ethSigner, pageSize);
 
     console.log('projects', projects);
