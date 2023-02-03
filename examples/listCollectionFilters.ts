a7f50acf1e520aeefa18fa48cb30f1d1e4c35be5
cdrabner95 <chris.drabner@immutable.com>
2023-02-03 18:30:00 -0500

    core-sdk examples for collections

---
+++ examples/listCollectionFilters.ts
@@ -0,0 +1,21 @@
+import { Config, ImmutableX, CollectionsApiListCollectionFiltersRequest } from '@imtbl/core-sdk';
+
+(async () => {
+  try {
+    // IMX class client
+    const client = new ImmutableX(Config.PRODUCTION);
+    
+    const collectionListFiltersRequest: CollectionsApiListCollectionFiltersRequest = {
+        pageSize: 200, // max size 200
+        address:'', // collection address
+        nextPageToken: ''// cursor
+    } 
+    
+    const collectionListFiltersResponse = await client.listCollectionFilters(collectionListFiltersRequest)
+
+    console.log('collectionListFiltersResponse', JSON.stringify(collectionListFiltersResponse, null, 4));
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
