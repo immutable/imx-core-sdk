bbda4510ba6a3230c16d051c47fbd91b1edce60c
Praveen Suluvai <praveen.suluvai@immutable.com>
2023-06-30 15:24:21 +1000

    DX-2184: Added tests to charecterize the legacy grind key implementation

--- src/utils/stark/starkCurve.test.ts
+++ src/utils/stark/starkCurve.test.ts
@@ -33,14 +33,36 @@ describe('Key generation', () => {
     );
   });
 
-  it('should generate Legacy Stark key', async () => {
+  it('Eth Private Key hashed with index once, should generate correct legacy stark key', async () => {
     const signer = new Wallet(
-      '3c71f6faf4e4a5347a602e3ca60c411caa856f2216dd94230ad4aa8d882a9ec9',
+      '0x1a245f2fa7c4f04a65d45a3877ad00b1423d081490dcc1a7050c8d7c11ec5c8f',
     );
     const starkKey = await generateLegacyStarkPrivateKey(signer);
     const starkPublicKey = createStarkSigner(starkKey).getAddress();
     expect(starkPublicKey).toEqual(
-      '0x046bdcc8686bf1c33daf9e8668ddb6e80c266ed8fc59cca8ab102f55838a80c2',
+      '0x034552d72951a700f6e105ac12f5b7977c178be0c32227281f2453b99f3af145', // Index 1
+    );
+  });
+
+  it('Eth Private Key hashed with index more than once, should generate correct legacy stark key', async () => {
+    const signer = new Wallet(
+      '0xe516a5b715dc53ba4bf06ed98cbe50897921f45f76d4138f8a7d4ba02a89e10d',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x06cf06df454bdbf37c14671939ff7192f54007e93781e1825e0d12eadb1bde29', // Index 2
+    );
+  });
+
+  it('Random Eth Private Key hashed with no index, should generate correct legacy stark key', async () => {
+    const signer = new Wallet(
+      '0x0ac63e2143a49a14a87d865c8d7993806ff16ac1c3288ff97101569881f0d306',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x06e6ac4bb44f3295b881532452f90eccb5601314fafe306db17684b47aa388bd', // Index 0
     );
   });
 });
