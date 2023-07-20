e68870923a8e0c258109cd2000426598afb2e0a7
Praveen Suluvai <Praveen.suluvai@immutable.com>
2023-07-20 12:51:46 +1000

    Grind key revert (#341)
    * porting legacy stark grind methods to core sdk
    
    * DX-2184: Passing tests for GrindKey legacy
    
    * DX-2184: Added tests to charecterize the legacy grind key implementation
    
    * Dx 2184 gk3 (#342)
    
    * Dx-2184: Rebasing the changes off gring-key-revert branch
    
    * DX-2184: First pass of Backwards Compatible logic
    Co-authored-by: Nik <CodeSchwert@users.noreply.github.com>
    
    * DX-2184: Added Tests for Backwards compatible key gen
    Co-authored-by: Nik <CodeSchwert@users.noreply.github.com>
    
    * DX-2184: Removed todo
    Co-authored-by: Nik <CodeSchwert@users.noreply.github.com>
    
    * DX-2184: ethAddress is non-optional now
    
    * DX-2184: Using 2.0.0 grindKey as the default version
    
    Co-authored-by: Carl Menezes <carleeto@users.noreply.github.com>
    Co-authored-by: Nik <CodeSchwert@users.noreply.github.com>
    
    * DX-2184: Added Changelog
    
    Co-authored-by: Carl Menezes <carleeto@users.noreply.github.com>
    Co-authored-by: Nik <CodeSchwert@users.noreply.github.com>
    
    * DX-2184: Added tests for legacy imx-sdk-js version
    
    * PR fix
    
    ---------
    
    Co-authored-by: Carl Menezes <carleeto@users.noreply.github.com>
    Co-authored-by: Nik <CodeSchwert@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Chris James <chris@webglowit.net>
    Co-authored-by: Carl Menezes <carleeto@users.noreply.github.com>
    Co-authored-by: Nik <CodeSchwert@users.noreply.github.com>

--- src/utils/stark/starkCurve.test.ts
+++ src/utils/stark/starkCurve.test.ts
@@ -3,9 +3,9 @@ import { Wallet } from 'ethers';
 import {
   generateLegacyStarkPrivateKey,
   generateStarkPrivateKey,
-  grindKey,
-  starkEcOrder,
 } from './starkCurve';
+import { grindKey } from './legacy/crypto';
+import { createStarkSigner } from './starkSigner';
 
 describe('Key generation', () => {
   it('should generate random Stark key', async () => {
@@ -21,6 +21,143 @@ describe('Key generation', () => {
       '0556413893a023efd75f62cd4eca825f2be7e918b5188f1db06cbec12d7d1b88',
     );
   });
+
+  it('should generate Legacy Stark public key', async () => {
+    const signer = new Wallet(
+      '5c7b4b5cad9a3fc7b1ba235a49cd74e615488a18b0d6a531739fd1062935104d',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x0579f97e8084dfbbead9bffd750df780e06d8c09a3ba7f40ebe51d46b47df043',
+    );
+  });
+
+  it('Random Eth Private Key hashed with no index, should generate correct legacy (imx-sdk-js) stark key', async () => {
+    const signer = new Wallet(
+      '0x4aee6bb7807a684341e39303fb1292f1c66b0f0bcac81407d10e80ba9202fc4a',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x3c35bb183cc613d57e6c823a6c7a17a14fba9dc65a5ece97a87020e32cbbd9', // Compatible with legacy.GrindKey, With Expected Index of 0,0,0,... // Hash Iterated 0 times
+    );
+  });
+
+  it('Eth Private Key hashed with index once, should generate correct legacy (imx-sdk-js) stark key', async () => {
+    const signer = new Wallet(
+      '0xabb730db6bd22a773263c9266b0e157e6a9985c75ff68c68fdebdef3e500892c',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x0743a17b8a566fe39cfebc41db631210bf328b91ae6c3968f1f9a96ac48a02aa', // Compatible with legacy.GrindKey, With Expected Index of 0,0,0,... // Hash Iterated 1 time
+    );
+  });
+
+  it('Eth Private Key hashed with index more than once, should generate correct legacy (imx-sdk-js) stark key', async () => {
+    const signer = new Wallet(
+      '0x2b9b32d0ed8863016001ffb15af781dca725b1581a3d4d61c0e2c4c0c03ede8a',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x07d14076863573a2310a948472512331416ceb655a304b1811a127e6c710e350', // Compatible with legacy.GrindKey, with Expected Index of 0,0,0,... // Hash Iterated 2 times
+    );
+  });
+
+  it('Eth Private Key hashed with index once, should generate Legacy Stark key backwards compatible with versions between 1.0.0-beta.3 to 2.0.0', async () => {
+    const signer = new Wallet(
+      '0x1a245f2fa7c4f04a65d45a3877ad00b1423d081490dcc1a7050c8d7c11ec5c8f',
+    );
+
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x07ca61905954bdd858ae63704b111da5ca52b30a21fa9aaeee9aef9b24e89607', // Compatible with GrindKeyV1, with Expected Index of 0,1,2,... // Hash Iterated 1 time
+    );
+  });
+
+  it('Eth Private Key hashed with index more than once, should generate Legacy Stark key backwards compatible with versions between 1.0.0-beta.3 to 2.0.0', async () => {
+    const signer = new Wallet(
+      '0xe516a5b715dc53ba4bf06ed98cbe50897921f45f76d4138f8a7d4ba02a89e10d',
+    );
+
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x0157c6efcb2c69604c1b46ffeb568b3b10ea7093da762e1daf66b59d9b63b5f9', // Compatible with GrindKeyV1, with Expected Index of 0,1,2,... // Hash Iterated 2 times
+    );
+  });
+
+  it('should generate Legacy Stark key backwards compatible with versions between 1.0.0-beta.3 to 2.0.0', async () => {
+    const signer = new Wallet(
+      'ba3c969f4957e6bf24e5cf8a931bdba4f90d27c01bb7dff738e4593142826db7',
+    );
+
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x04eb684f7318d2b90ef8703e6cd77e362414f4997934ee3c99ee50db3411dfef', // Compatible with GrindKeyV1, with Expected Index of 0,1,2,... // Hash Iterated 0 times
+    );
+  });
+
+  it('Eth Private Key hashed with index once, should generate Legacy Stark key backwards compatible with version 2.0.1', async () => {
+    const signer = new Wallet(
+      '0xa25022c521d884d195c119b705ac515fabb48971d3cf2369f188c96b0e6404ef',
+    );
+
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x03aa9edf4a5b33f623d3dd37a6df62e5590f172fd4d1a9483532464aa13bcbb9', // Compatible with core-sdk GrindKey, with Expected Index of 0,0,1,... // Hash Iterated 1 time
+    );
+  });
+
+  it('Eth Private Key hashed with index more than once, should generate Legacy Stark key backwards compatible with version 2.0.1', async () => {
+    const signer = new Wallet(
+      '0x719b531dfdbf5e327646ccd992923c9d8846d60767b49d0589d631c1f54d1f12',
+    );
+
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x057a8278637277ebaaa63b000185507eb847eec8f69c7758fc10a490d0226e7d', // Compatible with core-sdk GrindKey, with Expected Index of 0,0,1,... // Hash Iterated 2 times
+    );
+  });
+
+  it('Eth Private Key hashed with index once, should generate correct stark key', async () => {
+    const signer = new Wallet(
+      '0xe7ecb8f91175446248a6cfa45a9526c85bc4cd7cbd9427e3e65a82d3f5fb8cdc',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x05f8f00b03e896cb8c02a936bab73623fc3651fddabfe0d28fbc309c63642c10', // Hash Iterated 1 time
+    );
+  });
+
+  it('Eth Private Key hashed with index more than once, should generate correct stark key', async () => {
+    const signer = new Wallet(
+      '0x82930dfa052b198828f70529148b02178b03515e910217dab32bf9fc046bc9d9',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x04d34c9f519b2a3538b02c4e939188b591065eb8c2f210277c6ed1de85b16fab', // Hash Iterated 2 times
+    );
+  });
+
+  it('Random Eth Private Key hashed with no index, should generate correct legacy stark key', async () => {
+    const signer = new Wallet(
+      '0x0ac63e2143a49a14a87d865c8d7993806ff16ac1c3288ff97101569881f0d306',
+    );
+    const starkKey = await generateLegacyStarkPrivateKey(signer);
+    const starkPublicKey = createStarkSigner(starkKey).getAddress();
+    expect(starkPublicKey).toEqual(
+      '0x06e6ac4bb44f3295b881532452f90eccb5601314fafe306db17684b47aa388bd', // Hash Iterated 0 times
+    );
+  });
 });
 
 describe('Key grinding', () => {
@@ -29,7 +166,7 @@ describe('Key grinding', () => {
       '86F3E7293141F20A8BAFF320E8EE4ACCB9D4A4BF2B4D295E8CEE784DB46E0519',
       16,
     );
-    expect(grindKey(privateKey, starkEcOrder)).toEqual(
+    expect(grindKey(privateKey.toString('hex'))).toEqual(
       '5c8c8683596c732541a59e03007b2d30dbbbb873556fe65b5fb63c16688f941',
     );
   });
@@ -38,26 +175,14 @@ describe('Key grinding', () => {
       'a978531943ad2e2a8af34e0e2a7d306dc99516d489be16e4ea2ee74c90a9d88f',
       16,
     );
-    expect(grindKey(privateKey, starkEcOrder)).toEqual(
+    expect(grindKey(privateKey.toString('hex'))).toEqual(
       '1e8108d99e74b769d6b998a5a41ff2745f0607496f2eed39abfd161837408e7',
     );
   });
   it('should produce the correct ground key when the key starts with zero', () => {
-    const privateKey = new BN(
-      '086F3E7293141F20A8BAFF320E8EE4ACCB9D4A4BF2B4D295E8CEE784DB46E051',
-      16,
-    );
-    expect(grindKey(privateKey, starkEcOrder)).toEqual(
-      '2b2c6db790a95ce05426c3d67247547f1a72d104fd5af24553d42b7557ab082',
-    );
-  });
-  it('should produce the correct ground key when the hex key is less than 64 chars', () => {
-    const privateKey = new BN(
-      //same as previous test, but without the leading zero
-      '86F3E7293141F20A8BAFF320E8EE4ACCB9D4A4BF2B4D295E8CEE784DB46E051',
-      16,
-    );
-    expect(grindKey(privateKey, starkEcOrder)).toEqual(
+    const privateKey =
+      '086F3E7293141F20A8BAFF320E8EE4ACCB9D4A4BF2B4D295E8CEE784DB46E051';
+    expect(grindKey(privateKey)).toEqual(
       '2b2c6db790a95ce05426c3d67247547f1a72d104fd5af24553d42b7557ab082',
     );
   });
--- src/utils/stark/starkCurve.ts
+++ src/utils/stark/starkCurve.ts
@@ -1,16 +1,15 @@
+import { Signer } from '@ethersproject/abstract-signer';
+import { splitSignature } from '@ethersproject/bytes';
+import * as legacy from './legacy/crypto';
+import { createStarkSigner } from './starkSigner';
 import hash from 'hash.js';
 import { curves, ec } from 'elliptic';
 import * as encUtils from 'enc-utils';
 import BN from 'bn.js';
-import { Signer } from '@ethersproject/abstract-signer';
-import { splitSignature } from '@ethersproject/bytes';
 import { hdkey } from 'ethereumjs-wallet';
-
-const DEFAULT_SIGNATURE_MESSAGE =
-  'Only sign this request if you’ve initiated an action with Immutable X.';
-const DEFAULT_ACCOUNT_APPLICATION = 'immutablex';
-const DEFAULT_ACCOUNT_LAYER = 'starkex';
-const DEFAULT_ACCOUNT_INDEX = '1';
+import { ImmutableX } from '../../ImmutableX';
+import { IMXError } from '../../types/errors';
+import { Config } from '../../config/config';
 
 /*
 Stark-friendly elliptic curve
@@ -58,6 +57,14 @@ export const starkEc = new ec(
   }),
 );
 
+const MAX_ALLOWED_VAL = () => {
+  const sha256EcMaxDigest = new BN(
+    '1 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000',
+    16,
+  );
+  return sha256EcMaxDigest.sub(sha256EcMaxDigest.mod(starkEcOrder));
+};
+
 // Create a hash from a key + an index
 function hashKeyWithIndex(key: string, index: number): BN {
   return new BN(
@@ -74,34 +81,182 @@ function hashKeyWithIndex(key: string, index: number): BN {
   );
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// DANGER: DO NOT MODIFY GRIND KEY / KEY GENERATION LOGIC.
+//////////////////////////////////////////////////////////////////////////////////////////////////
 /*
- This function receives a key seed and produces an appropriate StarkEx key from a uniform
- distribution.
- Although it is possible to define a StarkEx key as a residue between the StarkEx EC order and a
- random 256bit digest value, the result would be a biased key. In order to prevent this bias, we
- deterministically search (by applying more hashes, AKA grinding) for a value lower than the largest
- 256bit multiple of StarkEx EC order.
-
- https://github.com/starkware-libs/starkware-crypto-utils/blob/dev/src/js/key_derivation.js#L119
+The grindKeyV201 function is used to provide a backwards compatible way to re-generate keys if any of the accounts were created using v2.0.1 of core-sdk.
 */
-export function grindKey(keySeed: BN, keyValLimit: BN) {
-  const sha256EcMaxDigest = new BN(
-    '1 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000',
-    16,
-  );
-  const maxAllowedVal = sha256EcMaxDigest.sub(
-    sha256EcMaxDigest.mod(keyValLimit),
-  );
+export function grindKeyV201(keySeed: BN): string {
+  const maxAllowedVal = MAX_ALLOWED_VAL();
+
   // The key passed to hashKeyWithIndex must have a length of 64 characters
   // to ensure that the correct number of leading zeroes are used as input
   // to the hashing loop
   let key = hashKeyWithIndex(keySeed.toString('hex', 64), 0);
+
   // Make sure the produced key is devided by the Stark EC order, and falls within the range
   // [0, maxAllowedVal).
   for (let i = 0; key.gte(maxAllowedVal); i++) {
     key = hashKeyWithIndex(key.toString('hex'), i);
   }
-  return key.umod(keyValLimit).toString('hex');
+  return key.umod(starkEcOrder).toString('hex');
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// DANGER: DO NOT MODIFY GRIND KEY / KEY GENERATION LOGIC.
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+This grindKey function is the default logic to be used by all our SDKs including the imx-sdk-js. All the accounts 
+that were created using imx-sdk-js are to be supported by the legacy.grindKey function.
+
+This function receives a key seed and produces an appropriate StarkEx key from a uniform
+distribution. 
+
+Although it is possible to define a StarkEx key as a residue between the StarkEx EC order and a
+random 256bit digest value, the result would be a biased key. In order to prevent this bias, we
+deterministically search (by applying more hashes, AKA grinding) for a value lower than the largest
+256bit multiple of StarkEx EC order.
+
+https://github.com/starkware-libs/starkware-crypto-utils/blob/dev/src/js/key_derivation.js#L119
+
+The accounts that has been created with versions released between 1.0.0-beta.3 and 2.0.0 are fine as it uses this method. 
+Although this logic has been in place in core-sdk since the following commit, it was momentarily changed in version 2.0.1 to use gringKeyV201 method.
+https://github.com/immutable/imx-core-sdk/commit/3d9e35b4598784589bd7f121f26e105493196716
+*/
+export function grindKey(keySeed: BN) {
+  const maxAllowedVal = MAX_ALLOWED_VAL();
+  // The key passed to hashKeyWithIndex must have a length of 64 characters
+  // to ensure that the correct number of leading zeroes are used as input
+  // to the hashing loop
+  let key = hashKeyWithIndex(keySeed.toString('hex', 64), 0);
+
+  // Make sure the produced key is devided by the Stark EC order, and falls within the range
+  // [0, maxAllowedVal).
+  for (let i = 1; key.gte(maxAllowedVal); i++) {
+    key = hashKeyWithIndex(key.toString('hex'), i);
+  }
+  return key.umod(starkEcOrder).toString('hex');
+}
+
+// Check if the hash value of the the given PrivateKey falls above the starkEcOrder limit.
+// This function is only serving the context of DX-2184, used to determine if we need to validate the generated key
+// against the one recorded in IMX servers.
+export function checkIfHashedKeyIsAboveLimit(keySeed: BN) {
+  const maxAllowedVal = MAX_ALLOWED_VAL();
+  // The key passed to hashKeyWithIndex must have a length of 64 characters
+  // to ensure that the correct number of leading zeroes are used as input
+  // to the hashing loop
+  const key = hashKeyWithIndex(keySeed.toString('hex', 64), 0);
+  return key.gte(maxAllowedVal);
+}
+
+// Gets the account (stark public key) value of the requested user (ethAddress) for Production environment only.
+export async function getStarkPublicKeyFromImx(
+  ethAddress: string,
+): Promise<{ starkPublicKey: string; accountNotFound: boolean } | undefined> {
+  try {
+    const imxClient = new ImmutableX(Config.PRODUCTION);
+    // Query existing account value for the given user (ethAddress).
+    const user = await imxClient.getUser(ethAddress);
+    if (user.accounts.length > 0) {
+      return { starkPublicKey: user.accounts[0], accountNotFound: false };
+    }
+  } catch (err) {
+    if (err instanceof IMXError && err.code == 'account_not_found') {
+      return { starkPublicKey: '', accountNotFound: true }; // This means a new account. So lets use the value from default GrindKey function.
+    }
+    throw err;
+  }
+}
+
+export function getPrivateKeyFromPath(seed: string, path: string): BN {
+  const privateKey = hdkey
+    .fromMasterSeed(Buffer.from(seed.slice(2), 'hex')) // assuming seed is '0x...'
+    .derivePath(path)
+    .getWallet()
+    .getPrivateKey();
+  return new BN(privateKey);
+}
+
+async function getKeyFromPath(
+  seed: string,
+  path: string,
+  ethAddress: string,
+): Promise<string> {
+  const privateKeySeed = getPrivateKeyFromPath(seed, path);
+  const starkPrivateKey = grindKey(privateKeySeed);
+
+  // The following logic is added in response to bug found in DX-2167 and DX-2184.
+  // To provide a backwards compatible way to fetch keys across link/js SDK and Core SDK.
+
+  // Note: The issue we are addressing here is that if the hashKeyWithIndex value is above the limit, then we perform
+  //     hash again with index until it comes below the limit. But for the first time the index is not incremented in
+  //     imx-sdk-js where as it was in core-sdk that was the difference. For this reason it would have worked for cases
+  //     where the hashed value was less than the limit and fails only when it is above the limit.
+  //     Refer, https://immutable.atlassian.net/browse/DX-2167
+
+  // Same code as grindKey, required to check if the generated private key
+  // goes above the limit when hashed for first time to identify if we need to do backwards
+  // compatibility check:
+
+  // The bug only exists if the hashed value of given seed is above the stark curve limit.
+  if (!checkIfHashedKeyIsAboveLimit(privateKeySeed)) {
+    return starkPrivateKey;
+  }
+
+  // Check if the generated stark public key matches with the existing account value for that user.
+  // We are only validating for Production environment.
+  // For Sandbox account/key mismatch, solution is to discard the old account and create a new one.
+  const imxResponse = await getStarkPublicKeyFromImx(ethAddress);
+  // If the account is not found or account matches we just return the key pair at the end of this method.
+  // Only need to so alternative method if the account is found but the stark public key does not match.
+
+  // If the account is not found it is a new account, just return the Stark Private Key that is generated by grindKey function.
+  if (imxResponse?.accountNotFound) {
+    return starkPrivateKey;
+  }
+
+  // If the user account matches with generated stark public key user, just return Stark Private Key.
+  if (
+    imxResponse?.starkPublicKey ===
+    createStarkSigner(starkPrivateKey).getAddress()
+  ) {
+    return starkPrivateKey;
+  }
+
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // This is backwards compatible crypto (core-sdk) version 2.0.1
+
+  // If we are here, we found the account but did not match with the recorded user account.
+  // Lets try to use grindKeyV201 method from backwards compatible logic to generate a key and see if that matches.
+  const starkPrivateKeyV201Compatible = grindKeyV201(privateKeySeed);
+  if (
+    imxResponse?.starkPublicKey ===
+    createStarkSigner(starkPrivateKeyV201Compatible).getAddress()
+  ) {
+    return starkPrivateKeyV201Compatible;
+  }
+
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // This section is legacy crypto, compatible with imx-sdk-js released before 1.43.5
+
+  const privateKeyString = legacy.getPrivateKeyFromPath(seed, path);
+  const starkPrivateKeyLegacy = legacy.grindKey(privateKeyString);
+
+  if (
+    imxResponse?.starkPublicKey ===
+    createStarkSigner(starkPrivateKeyLegacy).getAddress()
+  ) {
+    return starkPrivateKeyLegacy;
+  }
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  // Account is found, but did not match with stark public keys generated by either grindKey or grindKeyV1 method.
+  // Will have to contact support for further investigation.
+  throw new Error(
+    'Can not deterministically generate stark private key - please contact support',
+  );
 }
 
 /**
@@ -109,43 +264,8 @@ export function grindKey(keySeed: BN, keyValLimit: BN) {
  * @returns the private key as a hex string
  */
 export function generateStarkPrivateKey(): string {
-  const keyPair = starkEc.genKeyPair();
-  return grindKey(keyPair.getPrivate(), starkEcOrder);
-}
-
-function getIntFromBits(
-  hex: string,
-  start: number,
-  end: number | undefined = undefined,
-): number {
-  const bin = encUtils.hexToBinary(hex);
-  const bits = bin.slice(start, end);
-  const int = encUtils.binaryToNumber(bits);
-  return int;
-}
-
-function getAccountPath(
-  layer: string,
-  application: string,
-  ethereumAddress: string,
-  index: string,
-): string {
-  const layerHash = hash.sha256().update(layer).digest('hex');
-  const applicationHash = hash.sha256().update(application).digest('hex');
-  const layerInt = getIntFromBits(layerHash, -31);
-  const applicationInt = getIntFromBits(applicationHash, -31);
-  const ethAddressInt1 = getIntFromBits(ethereumAddress, -31);
-  const ethAddressInt2 = getIntFromBits(ethereumAddress, -62, -31);
-  return `m/2645'/${layerInt}'/${applicationInt}'/${ethAddressInt1}'/${ethAddressInt2}'/${index}`;
-}
-
-function getKeyFromPath(seed: string, path: string): string {
-  const privateKey = hdkey
-    .fromMasterSeed(Buffer.from(seed.slice(2), 'hex')) // assuming seed is '0x...'
-    .derivePath(path)
-    .getWallet()
-    .getPrivateKey();
-  return grindKey(new BN(privateKey), starkEcOrder);
+  const keyPair = legacy.starkEc.genKeyPair();
+  return legacy.grindKey(keyPair.getPrivate('hex'));
 }
 
 /**
@@ -156,14 +276,14 @@ export async function generateLegacyStarkPrivateKey(
   signer: Signer,
 ): Promise<string> {
   const address = (await signer.getAddress()).toLowerCase();
-  const signature = await signer.signMessage(DEFAULT_SIGNATURE_MESSAGE);
+  const signature = await signer.signMessage(legacy.DEFAULT_SIGNATURE_MESSAGE);
   const seed = splitSignature(signature).s;
-  const path = getAccountPath(
-    DEFAULT_ACCOUNT_LAYER,
-    DEFAULT_ACCOUNT_APPLICATION,
+  const path = legacy.getAccountPath(
+    legacy.DEFAULT_ACCOUNT_LAYER,
+    legacy.DEFAULT_ACCOUNT_APPLICATION,
     address,
-    DEFAULT_ACCOUNT_INDEX,
+    legacy.DEFAULT_ACCOUNT_INDEX,
   );
-  const key = getKeyFromPath(seed, path);
+  const key = await getKeyFromPath(seed, path, address);
   return key.padStart(64, '0');
 }
