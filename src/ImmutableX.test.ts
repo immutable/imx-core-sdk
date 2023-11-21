2c35e6412cafe7ae290b1ef4eeb97c0558888f6c
Kai Hirota <34954529+kaihirota@users.noreply.github.com>
2023-11-22 10:59:31 +1100

    update test name

--- src/ImmutableX.test.ts
+++ src/ImmutableX.test.ts
@@ -2,7 +2,7 @@ import { Config } from './config';
 import { ImmutableX } from './ImmutableX';
 
 describe('ImmutableXClient', () => {
-  it('GET starkex contract version', async () => {
+  it('should return version and message', async () => {
     const client = new ImmutableX(Config.SANDBOX);
     const contractVersion = await client.getStarkExContractVersion();
 
