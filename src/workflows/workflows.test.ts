4463bfee56c3435e14ac9521efce5292fc95ae66
Kai Hirota <34954529+kaihirota@users.noreply.github.com>
2023-11-22 12:13:20 +1100

    update test

--- src/workflows/workflows.test.ts
+++ src/workflows/workflows.test.ts
@@ -12,24 +12,26 @@ describe('getContractVersion', () => {
   });
 
   it('should return contract version 3.0.3', async () => {
-    const workflows = new Workflows(Config.SANDBOX);
-    mockedAxios.get.mockResolvedValueOnce({
-      data: {
-        version: '3.0.3',
-        message: '',
-      },
+    mockedAxios.get.mockImplementation(async path => {
+      if (path.includes('starkex-contract-version')) {
+        return {
+          data: { version: '3.0.3', message: 'test' },
+        };
+      }
+      return { data: undefined };
     });
 
+    const workflows = new Workflows(Config.SANDBOX);
     const contractVersion = await workflows.getStarkExContractVersion();
     expect(contractVersion.data.version).toEqual('3.0.3');
   });
 
   it('should throw error on 404 response', async () => {
-    const workflows = new Workflows(Config.SANDBOX);
     mockedAxios.get.mockRejectedValueOnce(
       new Error('Request failed with status code 404'),
     );
 
+    const workflows = new Workflows(Config.SANDBOX);
     await expect(workflows.getStarkExContractVersion()).rejects.toThrowError(
       new Error('Request failed with status code 404'),
     );
